479d3d8dd9648bb44a4d31229af8534f974a60b8
sheenatechzera <sheenatechzera@gmail.com>
2025-10-27 16:16:11 +0530

    test

--- FinacPOS/Masters/frmCountersReport.Designer.cs
+++ FinacPOS/Masters/frmCountersReport.Designer.cs
@@ -48,7 +48,7 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.Black;
-            this.btnClose.Location = new System.Drawing.Point(357, 216);
+            this.btnClose.Location = new System.Drawing.Point(355, 216);
             this.btnClose.Margin = new System.Windows.Forms.Padding(1);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(67, 27);
